c23c0327bd47cca8ca3643d21a4d735a5aaff4b1
N <cubingniles@gmail.com>
2022-01-14 22:51:16 +0530

    fix-data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('algs2.json');
+const router = jsonServer.router('algs3.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000;
 
